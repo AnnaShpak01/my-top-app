2edc08de3c5c31740a896834dfd067a7acd965cd
AnnaShpak01 <elistana@i.ua>
2023-09-08 13:13:28 +0200

    Paragraph and Tag components
    added p-component and tags

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 import Image from 'next/image'
 import styles from './page.module.css'
 import { Metadata } from 'next'
-import { Button, Htag } from './components';
+import { Button, Htag, P, Tag } from './components';
 import Logo from '../public/vercel.svg';
 
 // export const metadata: Metadata = {
@@ -22,6 +22,13 @@ export default function Home(): JSX.Element {
       <Htag tag="h1">Text</Htag>
       <Button appearance='primary' arrow='right'>Button</Button>
       <Button appearance='ghost' arrow='down'>Button</Button>
+      <P size='l'>Large</P>
+			<P>Medium</P>
+			<P size='s'>Small</P>
+      <Tag size='s'>Ghost</Tag>
+			<Tag size='m' color='red'>Red</Tag>
+			<Tag size='s' color='green'>Green</Tag>
+			<Tag color='primary'>Green</Tag>
     </>
   )
 }
