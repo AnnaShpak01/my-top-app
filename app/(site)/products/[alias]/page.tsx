fe782364d80de3c5150fb752d701ca7ac1e7d2b0
AnnaShpak01 <elistana@i.ua>
2023-09-14 17:06:28 +0200

    Not found
    Check on not found page

--- app/(site)/products/[alias]/page.tsx
+++ app/(site)/products/[alias]/page.tsx
@@ -1,9 +1,17 @@
+import { getMenu } from '@/api/menu'
+import { getPage } from '@/api/page'
+import { TopLevelCategory } from '@/interfaces/page.interface'
 import { Metadata } from 'next'
+import { notFound } from 'next/navigation'
 
 export const metadata: Metadata = {
-  title: 'Page',
+  title: 'Страница',
 }
 
-export default function PageProducts({ params }: { params: { alias: string } }) {
-  return <div>Page with alias {params.alias}</div>
+export default async function PageProducts({ params }: { params: { alias: string } }) {
+  const page = await getPage(params.alias)
+  if (!page) {
+    notFound()
+  }
+  return <div>{page.title}</div>
 }
