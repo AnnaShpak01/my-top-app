598bc9c8012be56bb83fe9198680ef327a8c6900
AnnaShpak01 <elistana@i.ua>
2023-09-14 17:11:08 +0200

    GenerateStaticParams
    generateStaticParams

--- app/(site)/products/[alias]/page.tsx
+++ app/(site)/products/[alias]/page.tsx
@@ -5,7 +5,12 @@ import { Metadata } from 'next'
 import { notFound } from 'next/navigation'
 
 export const metadata: Metadata = {
-  title: 'Страница',
+  title: 'Page',
+}
+
+export async function generateStaticParams() {
+  const menu = await getMenu(0)
+  return menu.flatMap((item) => item.pages.map((page) => ({ alias: page.alias })))
 }
 
 export default async function PageProducts({ params }: { params: { alias: string } }) {
